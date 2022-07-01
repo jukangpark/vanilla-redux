16ec246f5865d25046a77d53d609d3cd9f6a192c
jukangpark <skyxxx9339@naver.com>
2022-07-01 11:16:15 +0900

    바닐라 리덕스

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-import { createStore } from "redux";
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("number");
 
-const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
-  }
+let counter = 0;
+
+number.innerText = counter;
+
+const updateText = () => {
+  number.innerText = counter;
 };
 
-const countStore = createStore(countModifier);
+const handleAdd = () => {
+  counter++;
+  updateText();
+};
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const handleMinus = () => {
+  counter--;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+// import { createStore } from "redux";
+
+// const countModifier = (count = 0, action) => {
+//   console.log(count, action);
+//   if (action.type === "ADD") {
+//     return count + 1;
+//   } else if (action.type === "MINUS") {
+//     return count - 1;
+//   } else {
+//     return count;
+//   }
+// };
+
+// const countStore = createStore(countModifier);
+
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "MINUS" });
+// console.log(countStore.getState());
